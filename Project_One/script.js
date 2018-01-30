92648001d1e7333a6159ae763919ba5ae2100aa0
VDzhurkova <velpr.vd@gmail.com>
2018-01-30 16:12:38 +0000

    Add files via upload

---
+++ Project_One/script.js
@@ -0,0 +1,84 @@
+var company= [
+
+	{
+		name: "Fireclad Ltd",
+		address: {
+					street: "120 Old Broad Street",
+					city: "London",
+					postcode: "EC2N 1AR"
+		}
+	},
+
+	{
+		name: "Jarvis Ltd",
+		address: {
+					street: "10 Artiellery Passage",
+					city: "London",
+					postcode: "E1 7LU"
+		}
+	},
+
+	{
+		name: "MG Drywall",
+		address: {
+					street: "14 Finchley Park",
+					city: "London",
+					postcode: "N12 9JN"
+		}
+	}
+
+]
+
+document.getElementById("selectName").addEventListener("load", fillDrowDown());
+
+function fillDrowDown(){
+
+		for (var i=0; i<company.length; i++){
+				var companyName = company[i].name;
+				var select =  document.getElementById("selectName");
+				options = document.createElement("option");
+				options.textContent = companyName;
+				options.value = companyName;
+				select.appendChild(options);
+				console.log(options.value);
+
+		}
+}
+
+//document.getElementById("selectName").addEventListener("change", showAddress());
+
+function showAddress() {
+
+		var select = document.getElementById("selectName");
+		var option = select.options[select.selectedIndex];
+		console.log(option.value);
+		for (var i=0; i<company.length; i++){
+
+			 if (option.value == company[i].name)
+				{
+					document.getElementById("street-input").value = company[i].address.street;
+					document.getElementById("town-input").value = company[i].address.city;
+					document.getElementById("postcode-input").value = company[i].address.postcode;
+				}
+
+	}
+		}
+
+	function calculateInvoice(input) {
+		var input = document.getElementById("laborTotal")[0].value;
+		var total = parseInt(input, 10);
+		console.log(total);
+		var vat = input*(20/100);
+		console.log(vat);
+		var subTotal = input+vat;
+		var less20 = vat;
+		var totaldue = subTotal - less20;
+		document.getElementById("totalDue").innerHTML = totaldue;
+		console.log(totaldue);
+
+}
+
+
+
+
+
